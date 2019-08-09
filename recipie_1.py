4077062eb31096b7e654e9c28ee0ea27d4ec00f7
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-08-09 19:49:56 +0900

    Initial Commit

---
+++ recipie_1.py
@@ -0,0 +1,8 @@
+from sklearn import tree
+# 0 = bumpy 1 = smooth
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+# 0 = apples 1 = oranges
+labels = [0, 0, 1, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print(clf.predict([[150,0]]))
